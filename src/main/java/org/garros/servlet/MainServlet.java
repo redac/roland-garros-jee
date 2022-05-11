f1e602ce298b23dd94e89448720c848b09e1da1f
reda <20306749+redac@users.noreply.github.com>
2022-05-11 21:31:53 +0200

    feat: add main servlet

--- src/main/java/org/garros/servlet/MainServlet.java
+++ src/main/java/org/garros/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package org.garros.servlet;
 
+import java.io.IOException;
+
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,29 +14,12 @@ import jakarta.servlet.http.HttpSession;
 public class MainServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private PlayerService playerService = new PlayerServiceImpl();
+
 
 	private void doProcess(HttpServletRequest request, HttpServletResponse response) {
 
-		String searchText = request.getParameter("searchText");
-
 		HttpSession session = request.getSession();
-		@SuppressWarnings("unchecked")
-		List<String> searchHistory = (List<String>) session.getAttribute("SearchHistory");
-		if (searchHistory == null) {
-			searchHistory = new ArrayList<>();
-			session.setAttribute("SearchHistory", searchHistory);
-		}
-		searchHistory.add(searchText);
-
-		List<Player> players = new ArrayList<Player>();
-		if (searchText == null) {
-			players = playerService.getAllPlayers();
-		} else {
-			players = playerService.getPlayerByName(searchText);
-		}
-		request.setAttribute("listPlayers", players);
-
+	
 		String pageName = "/playerlist.jsp";
 		RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
 		try {
